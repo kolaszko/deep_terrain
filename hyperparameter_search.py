f1fce65b202633ebc49dce41763cfa09bedfa6d7
kolaszko <mikolaj.lysakowski.bk@gmail.com>
2023-04-14 14:29:58 +0200

    Remove pruner, fix TransformerEncoder due to PyTorch version

--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -22,7 +22,7 @@ def objective(trial, args, algorithm):
         log_model_checkpoints=False)
 
     trainer = pl.Trainer(max_epochs=args.max_epochs, callbacks=[
-        PyTorchLightningPruningCallback(trial, monitor="val/accuracy"),
+        EarlyStopping(monitor="val/accuracy", min_delta=0.00, patience=50, verbose=True, mode="max"),
         LearningRateMonitor(logging_interval='epoch')], logger=logger,
         accelerator='gpu' if torch.cuda.is_available() else 'cpu', devices=1, log_every_n_steps=2)
 
@@ -88,8 +88,7 @@ def optuna_pipeline(args):
     print(algorithms)
 
     for algo in algorithms:
-        pruner = optuna.pruners.MedianPruner()
-        study = optuna.create_study(direction="maximize", pruner=pruner)
+        study = optuna.create_study(direction="maximize")
         study.optimize(lambda trial: objective(trial, args, algo), n_trials=100, timeout=600)
         rerun_best_trial(study.best_trial, args, algo)
 
